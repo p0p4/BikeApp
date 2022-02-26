049d0a78be960a1e6f698d18f473c7bdb81f6407
niklasmalmgren <niklasam@metropolia.fi>
2022-02-26 14:31:20 +0200

    Small changes in weather.js

--- js/weather.js
+++ js/weather.js
@@ -9,6 +9,38 @@ const options = {
 function success(pos) {
   const crd = pos.coords;
 
+  sessionStorage.setItem("pos_lat",crd.latitude);
+  sessionStorage.setItem("pos_lon",crd.longitude);
+
+  document.querySelector('#value').innerHTML = `now`;
+
+    getWeather().then(function(weather) {
+      document.querySelector('#temp').innerHTML = `Temperature: ${weather.hourly[0].temp} C`;
+      document.querySelector(
+          '#main').innerHTML = `${weather.hourly[0].weather[0].main}`;
+      document.querySelector(
+          '#description').innerHTML = `Description: ${weather.hourly[0].weather[0].description}`;
+    });
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function getWeather () {
+  const proxy = 'https://api.allorigins.win/get?url=';
+  const key = '2c6ab1bfb47205e78b859594f1986b0e';
+  const search = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
+  const url = proxy + encodeURIComponent(search);
+
+  return fetch(url).then(function(response) {
+    return response.json();
+  }).then(function (data) {
+    return JSON.parse(data.contents);
+  });
+}
+
+function input (){
   const value = document.querySelector('#myRange').value;
 
   if (value == 0) {
@@ -17,35 +49,15 @@ function success(pos) {
     document.querySelector('#value').innerHTML = `in ${value}h`;
   }
 
-    getWeather(crd).then(function(weather) {
-      document.querySelector('#temp').innerHTML = `Temperature: ${weather.hourly[value].temp} C`;
-      document.querySelector(
-          '#main').innerHTML = `${weather.hourly[value].weather[0].main}`;
-      document.querySelector(
-          '#description').innerHTML = `Description: ${weather.hourly[value].weather[0].description}`;
-    });
-}
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-function getWeather (crd) {
-  const proxy = 'https://api.allorigins.win/get?url=';
-  const key = '2c6ab1bfb47205e78b859594f1986b0e';
-  const search = `https://api.openweathermap.org/data/2.5/onecall?lat=${crd.latitude}&lon=${crd.longitude}&appid=${key}&units=metric`;
-  const url = proxy + encodeURIComponent(search);
-
-  return fetch(url).then(function(response) {
-    return response.json();
-  }).then(function (data) {
-    const weather = JSON.parse(data.contents);
-    return weather;
+  getWeather().then(function(weather) {
+    document.querySelector('#temp').innerHTML = `Temperature: ${weather.hourly[value].temp} C`;
+    document.querySelector(
+        '#main').innerHTML = `${weather.hourly[value].weather[0].main}`;
+    document.querySelector(
+        '#description').innerHTML = `Description: ${weather.hourly[value].weather[0].description}`;
   });
 }
 
 navigator.geolocation.getCurrentPosition(success, error, options);
-
-function input (){
-  navigator.geolocation.getCurrentPosition(success, error, options);
-}
+const latitude = parseFloat(sessionStorage.getItem("pos_lat"));
+const longitude = parseFloat(sessionStorage.getItem("pos_lon"));
