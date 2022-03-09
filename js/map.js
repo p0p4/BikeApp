d502344c49441333eb56303815443d8aacfeced3
p0p4 <octet.item.0d@icloud.com>
2022-03-09 16:26:53 +0200

    refresh icon; minor changes to map and html

--- js/map.js
+++ js/map.js
@@ -110,10 +110,6 @@ const trackControl = L.Control.extend({
 }});
 map.addControl(new trackControl());
 
-map.on('mousedown touchstart', () => {
-  if (track) toggleTrack();
-});
-
 //#########################################
 //toggles bikestation visibility on the map
 let hideBikes = true;
